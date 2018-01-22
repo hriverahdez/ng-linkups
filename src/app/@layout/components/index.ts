cab18c54565d88af71d9f62bbe472743e053a2f5
Helian Rivera <hriverahdez2011@gmail.com>
2018-01-22 02:30:23 -0500

    Add app footer

--- src/app/@layout/components/index.ts
+++ src/app/@layout/components/index.ts
@@ -1,13 +1,16 @@
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { NavDropdownComponent } from "./nav-dropdown/nav-dropdown.component";
 import { NavTogglerComponent } from "./nav-toggler/nav-toggler.component";
+import { FooterComponent } from "./footer/footer.component";
 
 export const components: any[] = [
   SidebarComponent,
   NavDropdownComponent,
-  NavTogglerComponent
+  NavTogglerComponent,
+  FooterComponent
 ];
 
 export * from "./sidebar/sidebar.component";
 export * from "./nav-dropdown/nav-dropdown.component";
 export * from "./nav-toggler/nav-toggler.component";
+export * from "./footer/footer.component";
