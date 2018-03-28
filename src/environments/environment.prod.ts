e9afe918a3bc8ea15887bcfac05d561895c963de
Helian Rivera <hriverahdez2011@gmail.com>
2018-03-28 14:15:54 -0400

    Adding environment prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: "https://linkups-backend.herokuapp.com/api",
+  wsUrl: "https://linkups-backend.herokuapp.com",
+  masterKey: "sBRgpguuiVwdF4GyIEkzBAdXzj94i1gr"
 };
